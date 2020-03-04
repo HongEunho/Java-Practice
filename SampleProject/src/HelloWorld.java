1d20eabd62a03bb1842edd777028f9ba840fea35
HongEunho <46186664+HongEunho@users.noreply.github.com>
2020-03-04 23:45:52 +0900

    연습1

---
+++ SampleProject/src/HelloWorld.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		int []n = {1,2,3,4,5};
+		int sum=0;
+		for(int k:n)
+		{
+			sum+=k;
+		}
+		System.out.println("гую╨ "+sum);
+	}
+}
