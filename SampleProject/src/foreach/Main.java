bb8987d72f43633e96ee731ef400be9405975a56
HongEunho <46186664+HongEunho@users.noreply.github.com>
2020-03-05 00:21:59 +0900

    Commit

---
+++ SampleProject/src/foreach/Main.java
@@ -0,0 +1,18 @@
+package foreach;
+
+public class Main {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        int []n={1,2,3,4,5};
+        int sum=0;
+        for(int k:n){
+            sum+=k;
+        }
+        System.out.println("гую╨ "+sum);
+    }
+
+}
